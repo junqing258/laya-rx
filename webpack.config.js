1eb7a254c015f1fcc92c9428e46f6c1dd69591fe
ZHANGJUNQING <junqing258@126.com>
2018-08-24 00:21:07 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 const path = require('path');
 const os = require('os');
 const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const WebpackNotifierPlugin = require('webpack-notifier');
 const webpackDevServer = require('webpack-dev-server');
 const localServer = require('./server/index');
 
 
 var env = process.env.NODE_ENV || 'development';
-
 var definePlugin = new webpack.DefinePlugin({
-    __DEV__: env!=="production"
+    __DEV__: env !== "production"
 });
 
 var plugins = [
@@ -17,52 +17,51 @@ var plugins = [
     new WebpackNotifierPlugin()
 ];
 
-
-var plugins2 = [
-    new webpack.optimize.UglifyJsPlugin({
-        drop_console: true,
-        compress: false,
-        mangle: false,
-        output: {
-            comments: false,
-        },
-        beautify: false,
-        sourceMap: true,
-        test: /js\/.*\.js($|\?)/i
-    })
-];
-
-/* if (process.env.NODE_ENV === 'devserver') {
-    plugins.push(new webpack.HotModuleReplacementPlugin());
-} */
-if (process.env.NODE_ENV === 'production') {
-    plugins = plugins.concat(plugins2);
-}
-
 /**************************************************************************************************************************
  **
  */
 module.exports = {
+    mode: process.env.NODE_ENV,
     entry: {
-        app: [  path.resolve(__dirname, 'src/main.js') ]
+        app: [path.resolve(__dirname, 'src/main.js')]
     },
     devtool: 'source-map',
     output: {
         pathinfo: false,
-        path: path.resolve(__dirname, './bin'), 
+        path: path.resolve(__dirname, './bin'),
         filename: 'js/bundle.js'
     },
-    watch:  env!=="production",
+    watch: env !== "production",
+    // optimization: {
+    //     minimize: true,
+    //     minimizer: [
+    //         new UglifyJsPlugin({
+    //             drop_console: true,
+    //             compress: true,
+    //             mangle: false,
+    //             output: {
+    //                 comments: false,
+    //             },
+    //             beautify: false,
+    //             sourceMap: false,
+    //             test: /js\/.*\.js($|\?)/i
+    //         })
+    //     ]
+    // },
     plugins: plugins,
     module: {
-        rules: [ { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') } ]
+        rules: [{
+            test: /\.js$/,
+            use: ['babel-loader'],
+            include: path.join(__dirname, 'src')
+        }]
     },
     devServer: {
         hot: false,
         host: (function() {
             let wlan = os.networkInterfaces()['WLAN'];
-            if (wlan) return wlan[1]['address'];
-            else return null;
+            // if (wlan) return wlan[1]['address'];
+            // else return null;
         })(),
         disableHostCheck: true,
         contentBase: "./bin/",
@@ -72,11 +71,11 @@ module.exports = {
     },
     resolve: {
         modules: [
-          path.resolve('./src'),
-          path.resolve('./node_modules')
-        ], 
+            path.resolve('./src'),
+            path.resolve('./node_modules')
+        ],
         alias: {
 
         }
-  }
+    }
 };
