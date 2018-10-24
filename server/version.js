e068a1fa42ed8a75c8dc069118cf8508505a6b05
ZHANGJUNQING <junqing258@126.com>
2018-10-25 01:56:36 +0800

    commit

--- server/version.js
+++ server/version.js
@@ -2,76 +2,117 @@ const fs = require('fs');
 const path = require('path');
 const async = require('async');
 const child_process = require('child_process');
-const chalk  = require('chalk');
+const chalk = require('chalk');
 const fsextra = require('fs-extra');
 
-const root_path   = path.resolve(__dirname, '../');
-const bin_path    = path.resolve(__dirname, '../bin');
-const assets_path = path.resolve(__dirname, '../laya/assets');
-const publish_dir = fs.readFileSync(root_path+'/publish/gamehall').toString();
+const root_path = path.resolve(__dirname, '../');
+const bin_path = path.resolve(__dirname, '../bin/');
+const atlas_dir = 'res/atlas/';
+const rec_path = bin_path + '/' + atlas_dir + '.rec';
+const suffix = 'atlas';
+
+const publish_dir = fs.readFileSync(root_path + '/gamehall').toString();
 
 start();
 
 async function start() {
 	let asset_map = await getAtlas();
 	let commit_id = await getCommitId();
-	let last_commit_id = fs.readFileSync(bin_path+'/commint').toString();
-	let diff_list;
-	if (!['', 'null', 'undefined'].includes(last_commit_id)) {
+	let last_commit_id = await getLastCommitId().catch(e=> {
+		// console.log(e);
+	});
+
+	let diff_list = [];
+	if (last_commit_id) {
 		diff_list = await getDiff(last_commit_id);
 	}
 	let commit_list = filterAsset(asset_map, diff_list);
 
-	async.eachSeries(commit_list, (file_name, done)=> {
-		fsextra.copy(bin_path+'/'+file_name, publish_dir+file_name)
-			.then(() => {
-				console.log(chalk.green(`copy success: ${file_name}`));
-			}).catch(err => {
-				console.log(chalk.red(`copy failed: ${file_name}`));
-			}).finally(done);
+	async.eachSeries(commit_list, (item, done)=> {
+		let { type, name, atlas } = item;
+		let atlas_str = atlas ? `${atlas_dir}` : '';
+		let p = `${atlas_str}${name}`;
+		switch (type) {
+			case 'M':
+			case 'A':
+				fsextra.copy(`${bin_path}/${p}`, `${publish_dir}/${p}`)
+					.then(() => {
+						console.log(chalk.green(`copy success: ${p}`));
+					}).catch(err => {
+						console.log(chalk.red(`copy failed: ${p}`));
+					}).finally(done);
+				break;
+			case 'D':
+				fsextra.remove(`${publish_dir}/${p}`)
+					.then(() => {
+						console.log(chalk.green(`remove success: ${p}`));
+					}).catch(err => {
+						console.log(chalk.red(`remove failed: ${p}`));
+					}).finally(done);
+				break;
+			default:
+				done();
+				break;
+		}
+		// }
 	}, err=> {
-		fs.writeFile(bin_path+'/commint', commit_id, (err)=> {
+		fs.writeFile(bin_path + '/commint', commit_id, (err)=> {
 			if (err) throw err;
-			console.log(chalk.green(`commint id: ${commit_id}`));
+			console.log(chalk.green.bold(`\ncommint id: ${commit_id}\n`));
 		});
 	});
-	
-}
 
-function filterAsset(asset_map, diff_list) {
-	let commit_list = [];
-	if (!diff_list) {
-		return convertAssetList(asset_map);
-	}
-	diff_list.forEach(item=> {
-		if (asset_map[item] && asset_map[item].atlas) {
-			let key = asset_map[item].key;
-			commit_list.push(key+'.json', key+'.png');
-		} else if (/^(laya\/)/.test(item)) {
-			commit_list.push(item);
-		}
-	});
-	return commit_list;
 }
 
 function convertAssetList(asset_map) {
 	let asset_list = [];
-	Object.keys(asset_map).forEach(item=> {
-		let value = asset_map[item];
-		if (!value.atlas) {
-			asset_list.push(item);
-		} else if (!asset_list.includes(item)) {
-			let key = asset_map[item].key;
-			asset_list.push(key+'.json', key+'.png');
+	let altas_temp = [];
+	Object.keys(asset_map).forEach(name=> {
+		let value = asset_map[name];
+		let { atlas } = value;
+		let type = 'A';
+		if (!atlas) {
+			asset_list.push({ type, name, atlas });
+		} else if (!altas_temp.includes(name)) {
+			let key = asset_map[name].key;
+			altas_temp.push(name);
+			asset_list.push({ type, name: `${key}.png`, atlas });
+			asset_list.push({ type, name: `${key}.${suffix}`, atlas });
 		}
 	});
 	return asset_list;
 }
 
+function filterAsset(asset_map, diff_list) {
+	let commit_list = [];
+	if (!diff_list.length) {
+		return convertAssetList(asset_map);
+	}
+	let altas_temp = [];
+	diff_list.forEach(item=> {
+		let type = item.charAt(0);
+		let name = item.substr(2);
+		let atlas = 0;
+		if (/^(laya\/assets\/)/.test(name)) {
+			name = name.replace(/^(laya\/assets\/)/, '');
+			if (!altas_temp.includes(name) && asset_map[name] && asset_map[name].atlas) {
+				let key = asset_map[name].key;
+				atlas = 1;
+				type = 'M';
+				altas_temp.push(name);
+				commit_list.push({ type, name: `${key}.png`, atlas });
+				commit_list.push({ type, name: `${key}.${suffix}`, atlas });
+			} else {
+				commit_list.push({ type, name, atlas });
+			}
+		}
+	});
+	return commit_list;
+}
 
 async function getAtlas() {
 	return new Promise((relove, reject)=> {
-		let rec = fs.readFileSync(bin_path+'/.rec').toString().split('\n');
+		let rec = fs.readFileSync(rec_path).toString().split('\n');
 
 		let regD = /^D\ (.+)/;
 		let regP = /^P\ (.+)\ (.+\.png)/;
@@ -85,7 +126,7 @@ async function getAtlas() {
 				let atlas = 0;
 				let e = regP.exec(str);
 				if (e) atlas = 1;
-				else  e = regR.exec(str);
+				else e = regR.exec(str);
 				if (e) {
 					let name = `${key}/${e[2]}`;
 					let v = e[1];
@@ -115,13 +156,23 @@ async function getCommitId() {
 async function getDiff(last_commit_id) {
 	return new Promise((relove, reject)=> {
 		let diff_list;
-		let git_diff = child_process.spawn('git', ['diff', last_commit_id, '--name-only']);
+		let git_diff = child_process.spawn('git', ['diff', last_commit_id, '--name-status']); //
 		git_diff.stdout.on('data', (data) => {
 			diff_list = data.toString().split('\n');
 			diff_list = diff_list.map(v=> v.trim());
 		});
 		git_diff.on('close', ()=> {
 			relove(diff_list);
+		});
+	});
+}
+
+
+async function getLastCommitId() {
+	return new Promise((resolve, reject)=> {
+		fs.readFile(bin_path + '/commint', (err, data) => {
+			if (err) reject(err);
+			else resolve(data.toString());
 		});
 	});
 }
