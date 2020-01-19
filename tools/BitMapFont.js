a19b7d7956c37d3b740d40091c2118bca9d65163
junqing258 <1551486328@qq.com>
2020-01-19 14:15:47 +0800

    commander

--- tools/BitMapFont.js
+++ tools/BitMapFont.js
@@ -1,6 +1,9 @@
-
+const path    = require("path");
+const program = require("commander");
+const fs      = require("fs");
+const spawn   = require("child_process").spawn;
+const os      = require("os");
 const template = require('art-template');
-const fs = require("fs");
 
 const fnt = template(__dirname + '/tpl/tpl-fnt.art', {
 	files: [1,2,3,4]
